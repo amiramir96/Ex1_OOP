a270dbb81a0b6ba4608546c6466a288db6fc0e01
amiramir96 <89981387+amiramir96@users.noreply.github.com>
2021-11-10 13:59:28 +0200

    forgot to upload the random algorithm

---
+++ Brain/randomAlgo.py
@@ -0,0 +1,14 @@
+import random
+import Ex1Objects.Building
+import Ex1Objects.Elevator
+import Ex1Objects.CallForElevator
+
+class randomAlgo:
+    def __init__(self, Building):
+        self.building = Building
+
+    def run(self):
+        i=0
+        for x in self.building.getListOfCalls():
+            x.setAllocatedTo(random.randint(0, self.building.getNumberOfElevetors()-1))
+            i = i+1
